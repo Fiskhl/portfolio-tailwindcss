5abc5cedf46c407129796753da6bd0fe9db53313
Fiskhl <kafijaya@student.umn.ac.id>
2022-12-30 13:31:15 +0700

    portfolio tailwind css

---
+++ public/js/script.js
@@ -0,0 +1,80 @@
+// Navbar Fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
+};
+
+//Hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+//Mail JS
+(function () {
+  emailjs.init("_2AoIBFssiqefbJe8");
+})();
+
+function SendMail() {
+  var params = {
+    from_name: document.getElementById("name").value,
+    from_email: document.getElementById("email").value,
+    subject_email: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+  emailjs.send("service_vstlp4f", "template_vbhatad", params).then(
+    function (res) {
+      alert("Your message has been Sent!");
+    },
+    function (error) {
+      alert("FAILED...", error);
+    }
+  );
+}
+
+//Click outside Hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+//Dark Mode Toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+//Change toggle based on local storage
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
